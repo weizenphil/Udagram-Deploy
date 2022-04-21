cf21a7a1f3e2b683fc53d1888679e5a76b20d094
ayushban <ayushban@adobe.com>
2022-04-21 23:48:12 +0530

    Minor changes

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,13 +1,18 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
-  port: config.port,
+export const sequelize = new Sequelize(
+  'postgres://postgres:12345678@database-4.c3bnb9ymwlg9.us-east-1.rds.amazonaws.com:5432/postgres'
+);
 
-  dialect: "postgres",
-  storage: ":memory:",
-});
+
+// export const sequelize = new Sequelize({
+//   username: config.username,
+//   password: config.password,
+//   database: config.database,
+//   host: config.host,
+//   port: config.port,
+
+//   dialect: "postgres",
+//   storage: ":memory:",
+// });
