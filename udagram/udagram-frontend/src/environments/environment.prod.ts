83254eb4ccaec52a2a0768f7daa3d95c070b56a3
ayushban <ayushban@adobe.com>
2022-04-21 15:44:16 +0530

    EB support and CircleCI config filee

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  appName: 'udagram-api',
+  apiHost:
+    'http://udagram-api-dev.eba-i9khi2wn.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
